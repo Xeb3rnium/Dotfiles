6eca8da02b24b9effe595d0bde6aabcf941ed7ed
Xyb3rsky <12105433+Xeb3rnium@users.noreply.github.com>
2019-10-09 23:47:25 +0100

    Cleaned up errors

--- setup.sh
+++ setup.sh
@@ -12,27 +12,27 @@
 #Kali
 if [[ -n $(hostnamectl | grep "Operating System: Kali") ]];
 then
-   sudo rm ~/.bash_history
-   sudo mv .bash_aliases ~/
-   sudo echo "unset HISTFILE" >> .bashrc
-   sudo mv issue.net /etc/
-   sudo mv *.link /etc/systemd/network/
+   sudo cp .bash_aliases ~/
+   sudo echo "unset HISTFILE" >> ~/.bashrc
+   sudo cp issue.net /etc/
+   sudo cp *.link /etc/systemd/network/
    sudo update-initramfs -u
    sudo apt install realtek-rtl88xxau-dkms
 fi
 
 
 #Ubuntu
-if [[ -n $(hostnamectl | grep "Operating System: Ubuntu") ]];
+elif [[ -n $(hostnamectl | grep "Operating System: Ubuntu") ]];
 then
-   sudo rm ~/.bash_history
-   sudo mv .bash_aliases ~/
-   sudo echo "unset HISTFILE" >> .bashrc
-   sudo mv issue.net /etc/
-   sudo mv 01-network-manager-all.yaml /etc/netplan/
+   sudo cp .bash_aliases ~/
+   sudo echo "unset HISTFILE" >> ~/.bashrc
+   sudo cp issue.net /etc/
+   sudo cp 01-network-manager-all.yaml /etc/netplan/
    sudo netplan apply
    #sudo apt install rtl88au-dkms
 fi
 
+
 else
    echo "Unknown operating system";
+fi
