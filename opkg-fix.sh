583d1b9697702c45b7fb119b3c0d3f57382de6a7
Xyb3rsky <12105433+Xeb3rnium@users.noreply.github.com>
2019-12-18 03:22:36 +0000

    missing slash

--- opkg-fix.sh
+++ opkg-fix.sh
@@ -1,4 +1,5 @@
+# Dependencies fix for the wifi pineapple nano in relation to the micro sd card
 ln -s /sd/usr/lib/opkg/info/* /usr/lib/opkg/info/
 ln -s /sd/usr/bin/* /usr/bin/
-ln -s /sd/usr/sbin/* /usr/sbin
+ln -s /sd/usr/sbin/* /usr/sbin/
 ln -s /sd/usr/lib/* /usr/lib/
